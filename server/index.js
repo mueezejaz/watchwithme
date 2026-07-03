b6150c40275c5f39d1a7d088cb4c0748b20b2727
mueezejaz <mueezejaz12345678@gmail.com>
2026-07-03 16:10:02 +0500

    set up backend and websocket in both frontend and backend

--- server/index.js
+++ server/index.js
@@ -1,1 +1,44 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
+const port = 3001;
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.auth.userId;
+
+  console.log("New user:", userId);
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", userId);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello world");
+});
+
+server.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
