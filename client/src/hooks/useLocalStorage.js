ebd2097a5d0f9e12e0f4cb6e42f6eeda4503d1b3
mueezejaz <mueezejaz12345678@gmail.com>
2026-07-09 12:56:07 +0500

    comment the code in uselocalhost that get the value from the localstrage first. now it always get new value

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -3,12 +3,12 @@ import { useEffect, useRef, useState } from "react";
 const prefixKey = "sync_watch";
 
 function readValue(key, initialValue) {
-  try {
-    const jsonValue = localStorage.getItem(key);
-    if (jsonValue != null) return JSON.parse(jsonValue);
-  } catch {
-    console.log("some thing went wrong while getting local storage value");
-  }
+  // try {
+  //   const jsonValue = localStorage.getItem(key);
+  //   if (jsonValue != null) return JSON.parse(jsonValue);
+  // } catch {
+  //   console.log("some thing went wrong while getting local storage value");
+  // }
   return typeof initialValue === "function" ? initialValue() : initialValue;
 }
 
